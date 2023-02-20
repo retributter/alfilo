6e1ca0309059c432c3e14475b8ed3a2f75a9ab8d
root <alejandrobinarycode@gmail.com>
2023-02-20 22:01:40 +0100

    general re-do

--- backup.sh
+++ backup.sh
@@ -15,30 +15,29 @@ else
 fi
 }
 directoryCreator(){
-    logging="./log-cron/log-$varDate"
-    if [ -d "/root/log-cron/cron-$varDate" ]
+    touch ./log-cron/log-$varDate.md
+    chmod 775 ./log-cron/log-$varDate.md
+    logging="./log-cron/log-$varDate.md"
+    if [ -d "./log-cron/cron-$varDate" ]
     then 
-        mkdir /root/log-cron/cron-$varDate-old
-        chmod 775 /root/log-cron/cron-$varDate-old
-        cp -r /root/log-cron/cron-$varDate /root/log-cron/cron-$varDate-old/
-        rm -r /root/log-cron/cron-$varDate
+        mkdir ./log-cron/cron-$varDate-old
+        chmod 775 ./log-cron/cron-$varDate-old
+        cp -r ./log-cron/cron-$varDate /root/log-cron/cron-$varDate-old/
+        rm -r ./log-cron/cron-$varDate
         directoryCreator
     else
         mkdir cron-$varDate
         chmod 775 cron-$varDate
         mv cron-$varDate ./log-cron/
-        site="/root/log-cron/cron-$varDate"
+        site="./log-cron/cron-$varDate"
     fi
 cronSchedule
 }
     #what does the cron do:
     cronSchedule(){
-    zip data-$varDate.zip $varLoc/profiles $varLoc/sites $varLoc/sql-file.env $varLoc/docker-compose.yml $varLoc/docker-containerdrup
-    zip data-db-$varDate.zip $varLoc/docker/
-    chmod 775 data-$varDate.zip
-    chmod 775 data-db-$varDate.zip
-    mv data-$varDate.zip $site/data-$varDate.zip
-    mv data-db-$varDate.zip $site/data-db-$varDate.zip
+    zip -v -r docker-compose-$varDate.zip docker-compose >> $logging
+    chmod 775 docker-compose-$varDate.zip
+    mv docker-compose-$varDate.zip $site/docker-compose-$varDate.zip
     }
 #Back to main()    
 checkerLog
